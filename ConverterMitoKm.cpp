3bac53c47c159c5556511fd41079da1ce259cf6f
IvanIvanov-P <markelo98@mail.ru>
2024-08-22 11:19:12 +0300

    Test Commit

---
+++ ConverterMitoKm.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void milesToKilometres (double miles)
+{
+    double km = miles * 1.609;
+    cout << "Расстояние в километрах: " << km;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double distance;
+    cout << "Конвертер мили в километры.\n"
+        << "Введите расстояние в милях:";
+    cin >> distance;
+    milesToKilometres(distance);
+    return 0;
+}
